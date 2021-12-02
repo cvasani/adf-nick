30d3fff10ec00f156b0ac9d41c43f7bc2bb2af74
cvasani <vasani83@yahoo.com>
2021-12-02 15:12:20 +0530

    Chapter 2 : Recepie 2 : Complete

--- allnotes/sqls/FacesHR_SQls.sql
+++ allnotes/sqls/FacesHR_SQls.sql
@@ -1,2 +1,2 @@
-select max(employee_id) from EMPLOYEES;
-create sequence EMPLOYEES_SEQ start with 210 increment by 1 maxvalue 1000;
+--select max(employee_id) from EMPLOYEES;
+--create sequence EMPLOYEES_SEQ start with 210 increment by 1 maxvalue 1000;
